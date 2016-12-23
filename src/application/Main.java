ffbfc8e9d85658b6b7a4ffb381fafb99bae0ffdf
tkillman <dkkim1004@hanmail.net>
2016-12-23 16:01:52 +0900

    <충돌실험>

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+		
+			FXMLLoader fxml = FXMLLoader.load(getClass().getResource("main.fxml"));
+			Parent root = fxml.load();
+			
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/test5.java
@@ -0,0 +1,10 @@
+package application;
+
+public class test5 {
+
+	public static void main(String[] args) {
+		//충돌부분
+	}
+	
+	
+}
